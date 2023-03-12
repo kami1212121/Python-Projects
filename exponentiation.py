dd03775a8b635a957a48e88738adcde7ff32102f
Kamil <92430761+kami1212121@users.noreply.github.com>
2023-03-12 01:01:20 +0100

    Update exponentiation.py

--- exponentiation.py
+++ exponentiation.py
@@ -3,3 +3,13 @@ potega = int(input("Podaj potęge: "))
 def pot(x,y):
     return x ** y
 print(pot(liczba,potega))
+
+#2 Sposób wersja zooptymalizowana, sprawdzająca błędy
+try:
+    liczba = int(input("Podaj liczbe: "))
+    potega = int(input("Podaj potęge: "))
+    wynik = pow(liczba,potega)
+except ValueError:
+    print("Nie podano liczb")
+except:
+    print("Inny błąd")
